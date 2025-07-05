ed403579ebca33a60ee47691a1116ffab9dc0580
Wooseop Kim <wooseop@protonmail.com>
2025-07-06 07:47:49 +0900

    Add CKEditor component to Article blocks

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -422,7 +422,13 @@ export interface ApiArticleArticle extends Struct.CollectionTypeSchema {
   attributes: {
     author: Schema.Attribute.Relation<'manyToOne', 'api::author.author'>;
     blocks: Schema.Attribute.DynamicZone<
-      ['shared.media', 'shared.quote', 'shared.rich-text', 'shared.slider']
+      [
+        'shared.media',
+        'shared.quote',
+        'shared.rich-text',
+        'shared.slider',
+        'shared.ck-editor',
+      ]
     >;
     category: Schema.Attribute.Relation<'manyToOne', 'api::category.category'>;
     cover: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
